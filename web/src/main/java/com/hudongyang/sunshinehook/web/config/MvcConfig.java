a7448a0f1279e4c54b9858330bf9f31aca49a8e4
dongyang.hu <632433151@qq.com>
2021-02-17 20:49:48 +0800

    init

--- web/src/main/java/com/hudongyang/sunshinehook/web/config/MvcConfig.java
+++ web/src/main/java/com/hudongyang/sunshinehook/web/config/MvcConfig.java
@@ -3,15 +3,23 @@ package com.hudongyang.sunshinehook.web.config;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.hudongyang.sunshinehook.common.constants.BaseConstants;
+import com.hudongyang.sunshinehook.web.config.inteceptor.HookEventInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author dongyang.hu
  * @date 2021/2/5 18:27
  */
 @Configuration
-public class MvcConfig {
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private HookEventInterceptor hookEventInterceptor;
 
     @Bean
     public FastJsonHttpMessageConverter converter() {
@@ -23,5 +31,11 @@ public class MvcConfig {
                 SerializerFeature.WriteNullStringAsEmpty,
                 SerializerFeature.WriteNullBooleanAsFalse);
         return converter;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        String filterPath = "/" + BaseConstants.REQUEST_PREFIX + "/" + BaseConstants.HOOK_EVENT_URL;
+        registry.addInterceptor(hookEventInterceptor).addPathPatterns(filterPath);
     }
 }
